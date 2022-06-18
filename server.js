f03a4bbfd348595e852b095437cf4d29fa6c2fe3
malgorzataziolek <gosiaziolek1223@gmail.com>
2022-06-18 23:24:54 +0200

    add addTask

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
-
-const app = express();
 const socket = require('socket.io');
+const path = require('path');
+const app = express();
 
-const tasks = [];
+let tasks = [];
+
+app.use((req, res) => {
+	res.status(404).send({ message: 'Not found...' });
+});
 
 const server = app.listen(process.env.PORT || 8000, () => {
 	console.log('Server is running...');
 });
+
 const io = socket(server);
 
-app.use((req, res) => {
-	res.status(404).send({ message: 'Not found...' });
+io.on('connection', socket => {
+	console.log('New client ! Its id - ' + socket.id);
+	socket.emit('updateData', tasks);
+
+	socket.on('addTask', task => {
+		console.log(`Added task: ${task.name} id: ${task.id} by: ${socket.id}`);
+		tasks.push(task);
+		socket.broadcast.emit('addTask', task);
+	});
 });
